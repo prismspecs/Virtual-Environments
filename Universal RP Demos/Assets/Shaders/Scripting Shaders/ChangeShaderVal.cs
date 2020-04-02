01f3e2ae9dcbacb1934361a4f128b006bf0cc2da
Grayson Earle <graysonearle@gmail.com>
2020-04-02 15:13:31 -0400

    ...

---
+++ Universal RP Demos/Assets/Shaders/Scripting Shaders/ChangeShaderVal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShaderVal : MonoBehaviour
+{
+    // need a reference to this objects material, so we can communicate to shader
+    Material myMat;
+
+    // create a float (or bool, or whatever is needed)
+    // you can see which properties are exposed in the Matrix.shader file,
+    // in our case it looks like this:
+    /*
+    Properties
+	{
+		_Grid("Grid", range(1, 50.)) = 30.
+		_SpeedMax("Speed Max", range(0, 30.)) = 20.
+		_SpeedMin("Speed Min", range(0, 10.)) = 2.
+		_Density("Density", range(0, 30.)) = 5.
+	}
+    */
+    // so I can see that "_Grid" is an exposed property that is a float
+    // which is why I chose to use it. Also note that it wants values
+    // between 1 and 50, and defaults to 30
+    float myFloat = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set a reference to the renderer's material
+        myMat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player presses up or down arrow, increase or decrease float value
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            myFloat += 1.0f;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            myFloat -= 1.0f;
+
+        // use that for the _Grid property
+        // if you try this out in game mode you should see the shader
+        // change when you hit up/down
+        myMat.SetFloat("_Grid", myFloat);
+    }
+}
--- week11/readme.md
+++ week11/readme.md
@@ -28,6 +28,22 @@ Shaders Workshop
 2:00 - 2:40
 Show Shaders and Q&A
 
+## Thursday
+
+[Normal Mapping Explained - watch this first!](https://www.youtube.com/watch?v=SQrHkKnSBcA)
+
+[Zoom recording available here]()
+
+12:10 - 1:30
+More shader demos/workshop
+
+1:45 - 2:25
+Project checkins
+
+2:25 - 3:00
+Using downloaded (non Shader Graph) shaders
+Scripting for downloaded shaders
+
 ### Helpful Links and Resources
 
 + [What are Shaders? short vid](https://www.youtube.com/watch?v=sXbdF4KjNOc)
@@ -35,3 +51,9 @@ Show Shaders and Q&A
 + [Recreating a Zelda-inspired toon shading in Shader Graph](https://connect.unity.com/p/zelda-inspired-toon-shading-in-shadergraph)
 + [Another vertex displacement tutorial](https://www.youtube.com/watch?v=vh85pzT959M)
 + [Dissolve Effect shader tutorial](https://www.youtube.com/watch?v=taMp1g1pBeE)
+
+## Pre-made Shaders
++ [Shader Collection](https://github.com/michidk/Unity-Script-Collection#materials--shading)
++ [Shader Project](https://github.com/ellioman/ShaderProject)
++ [ShadersLab](http://shaderslab.com/shaders.html)
++ [Unity Shader Graph examples](https://github.com/UnityTechnologies/ShaderGraph_ExampleLibrary)
