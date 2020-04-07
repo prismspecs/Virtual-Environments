d6770a4a4bafc42f6cec8aa8d73b6841d4b95790
prismspecs <graysonearle@gmail.com>
2020-04-07 17:02:14 -0400

    ...

--- Unity Demos/Assets/Raycast/RaycastCheckForward.cs
+++ Unity Demos/Assets/Raycast/RaycastCheckForward.cs
@@ -7,8 +7,38 @@ public class RaycastCheckForward : MonoBehaviour
 
     public float ViewDistance = 3f;
 
+    private float MyRotation;
+
+    // the camera will rotate + or - the rotation amount
+    // from the offset position, so you can be more intentional
+    // with where it's looking
+    public float RotationAmount = 30f;
+    public float RotationOffset;
+
+    public float RotateSpeed = .1f;
+
+    // also have an angle offset to make the camera point
+    // up or down a bit like a real security cam
+    public float Tilt = 15f;
+
+    // bonus: setoff an alert light if player is seen
+    public Material AlertLight;
+
     void Update()
     {
+        // rotate the surveillance cube
+        // since sine waves go from -1 to 1, if we multiply it
+        // by 30, it will go from -30 degrees to positive 30 degrees
+        // we will make the rotation amount a public variable
+        MyRotation = Mathf.Sin(Time.time * RotateSpeed) * RotationAmount;
+
+        // to directly set the total rotation of a game object in degrees we
+        // can use transform.eulerAngles
+        // we want it to swivel around its Y axis so just multiply Vector3.up
+        // by MyRotation
+        transform.eulerAngles = new Vector3(Tilt, MyRotation + RotationOffset, 0f);
+        
+
         // create a vector that shoots out from the center of this object
         // we can create ViewDistance as a public float so we can change it
         // from the editor
@@ -22,10 +52,16 @@ public class RaycastCheckForward : MonoBehaviour
         if (Physics.Raycast(transform.position, forward, out hit, ViewDistance))
         {
             
-            if(hit.transform.name.Contains("Player"))
+            if(hit.transform.name.Contains("ThirdPersonController"))
             {
-                Debug.Log("I see the player~!");
-            }
+                Debug.Log("I see the player!");
+                AlertLight.EnableKeyword("_EMISSION");
+            } 
+        }
+        else
+        {
+            Debug.Log("I don't see anything...");
+            AlertLight.DisableKeyword("_EMISSION");
         }
     }
 }
