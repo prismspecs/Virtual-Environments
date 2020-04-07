ef9c3e310c1e4e92283ee1d23cbeeb57e7cad841
Grayson Earle <graysonearle@gmail.com>
2020-04-06 20:00:58 -0400

    ...

---
+++ Unity Demos/Assets/Raycast/RaycastCheckForward.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCheckForward : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 3;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(transform.position, forward, 10))
+            print("There is something in front of the object!");
+    }
+}
---
+++ week12/readme.md
@@ -0,0 +1,5 @@
+# Week12
+
+## Raycasting
+
++ To see drawn Debug lines in game, make sure that the Gizmos button is pressed in the Game tab
