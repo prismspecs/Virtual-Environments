af535aca9353f0cdabb43a1b3efd55eca0991dc6
Grayson Earle <graysonearle@gmail.com>
2020-04-06 20:11:44 -0400

    ...

--- Unity Demos/Assets/Raycast/RaycastCheckForward.cs
+++ Unity Demos/Assets/Raycast/RaycastCheckForward.cs
@@ -10,6 +10,16 @@ public class RaycastCheckForward : MonoBehaviour
         Debug.DrawRay(transform.position, forward, Color.green);
 
         if (Physics.Raycast(transform.position, forward, 10))
+        {
             print("There is something in front of the object!");
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100.0f))
+        {
+            print("Found an object - distance: " + hit.distance);
+            Debug.Log(hit.collider.name);
+        }
     }
 }
