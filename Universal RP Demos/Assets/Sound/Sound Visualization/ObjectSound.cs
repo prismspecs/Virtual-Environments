3ce67db2717796cb1657cb09b904610a0f13d512
Grayson Earle <graysonearle@gmail.com>
2020-04-22 19:53:40 -0400

    ...

---
+++ Universal RP Demos/Assets/Sound/Sound Visualization/ObjectSound.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+
+public class ObjectSound : MonoBehaviour
+{
+
+    public AudioClip MySound;
+    private AudioSource MyAudioSource;
+    public TextMesh MyTextPrefab;
+    public string MyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // where did we collide?
+        // you can either get the collision position of the first
+        // collision contact point in the array, which is easy
+        ContactPoint contact = collision.contacts[0];
+
+        // or if there are multiple collision points, you may want to
+        // find the average position
+        Debug.Log(collision.contacts.Length);
+
+        // create an empty Vector3 to store the sum of all collision points
+        Vector3 average = Vector3.zero;
+        // loop thru all points of collision and add them to the average
+        for (int i = 0; i < collision.contacts.Length; i++)
+        {
+            average += collision.contacts[i].point;
+        }
+        // then simply divide by the number of collisions to get the average
+        average = average / collision.contacts.Length;
+
+        // create the text object at the position of the collision
+        TextMesh go = Instantiate(MyTextPrefab, average, Quaternion.identity);
+
+        // change its text property
+        go.text = MyText;
+
+        // play the sound
+        MyAudioSource.PlayOneShot(MySound);
+        Destroy(go.gameObject, .5f);   // maybe it should go away quickly?
+
+        // perhaps we should also destroy this game object, as if it were a drop of water
+        Destroy(this.gameObject, .5f);
+
+    }
+
+}
---
+++ Universal RP Demos/Assets/Sound/Sound Visualization/SpawnInterval.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInterval : MonoBehaviour
+{
+
+    public GameObject SpawnThis;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Instantiate(SpawnThis, transform.position, Quaternion.identity);
+        }
+    }
+}
