65065c157a4429eb9451105dc3ed6cb482674508
Grayson Earle <graysonearle@gmail.com>
2020-04-29 22:43:03 -0400

    /

---
+++ Universal RP Demos/Assets/Footprints/Footprint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footprint : MonoBehaviour
+{
+
+    // "Life" of footprint will be used as alpha val, and to see if it should be deleted
+    float Life = 1;
+
+    // reference to material that will be automatically set up
+    Material MyMat;
+
+    // bonus! add a particle system effect
+    public GameObject ParticleSys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create a reference to the material so we can change shader properties
+        MyMat = GetComponent<Renderer>().material;
+
+        // spawn a particle system
+        GameObject ps = Instantiate(ParticleSys, transform);
+        Destroy(ps, 1f);    // destroy the particle system after a sec
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // decrease "life" over time, 50% per second
+        Life -= .5f * Time.deltaTime;
+
+        // use that as the alpha value for the shader
+        MyMat.SetFloat("_Alpha", Life);
+
+        // delete footprint if its time
+        if (Life <= 0f)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Universal RP Demos/Assets/Footprints/FootprintGen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootprintGen : MonoBehaviour
+{
+
+    public GameObject LeftFootprint;
+    public GameObject RightFootprint;
+
+    public Transform LeftFoot;
+    public Transform RightFoot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for our purposes, just move him forward
+        transform.Translate(Vector3.forward * Time.deltaTime);
+
+        transform.Rotate(Vector3.up * 22 * Time.deltaTime);
+    }
+
+    public void FootDown(int whichFoot)
+    {
+        // derive a position that is where the foot is but at Y = 0
+        Vector3 LeftFootAdjusted = new Vector3(LeftFoot.position.x, 0.01f, LeftFoot.position.z);
+        Vector3 RightFootAdjusted = new Vector3(RightFoot.position.x, 0.01f, RightFoot.position.z);
+
+        // get rotation of character...
+        Vector3 rot = transform.rotation.eulerAngles;
+
+        // my footprints need to be rotated by 180 degrees
+        rot = new Vector3(rot.x, rot.y + 180, rot.z);
+
+        if(whichFoot == 0)
+            Instantiate(LeftFootprint, LeftFootAdjusted, Quaternion.Euler(rot));
+        else
+            Instantiate(RightFootprint, RightFootAdjusted, Quaternion.Euler(rot));
+    }
+
+}
